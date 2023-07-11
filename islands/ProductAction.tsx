caadb19709611ab093bfb6dfd40d1cf6977edcb1
blkluv <info@blkluv.org>
2023-07-11 12:51:08 -0400

    hahz

--- islands/ProductAction.tsx
+++ islands/ProductAction.tsx
@@ -5,7 +5,7 @@ interface ProductActionProps {
 export default function ProductAction(props: ProductActionProps) {
   return (
     <a
-      href={`https://ads.arvrtise/ksm/gallery/${props.id}`}
+      href={`https://ads.arvrtise.com/ksm/gallery/${props.id}`}
       class={`w-full bg-gray-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-gray-900`}
       target={'_blank'}
     >
