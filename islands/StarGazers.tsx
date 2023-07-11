eaf4fb7655376c9d7599305433a39c17d1565469
blkluv <info@blkluv.org>
2023-07-11 12:25:45 -0400

    updated Discord

--- islands/StarGazers.tsx
+++ islands/StarGazers.tsx
@@ -1,16 +1,14 @@
 import IconGithub from "@/components/IconGithub.tsx";
 import IconDiscord from "@/components/IconDiscord.tsx";
-import {
-  useStargazers
-} from "@/utils/data.ts"
+import { useStargazers } from "@/utils/data.ts";
 
 export default function StarGazers() {
   const { data, error } = useStargazers();
 
   const add = (e: MouseEvent) => {
     e.preventDefault();
-    const url = `https://discord.gg/nfAmqTGNAU`;
-    window.open(url, '_blank')
+    const url = "https://discord.gg/nfAmqTGNAU";
+    window.open(url, "_blank");
   };
 
   if (error) {
@@ -19,16 +17,13 @@ export default function StarGazers() {
 
   return (
     <div>
-       <button
-  onClick={add}
-  class="flex items-center gap-2 items-center border-2 border-E6007E rounded-full px-5 py-1 font-semibold text-white hover:bg-E6007E hover:text-white transition-colors duration-300"
->
-  <IconDiscord />
-  {data?.watchers ?? "Discord"}
-</button>
-
-
+      <button
+        onClick={add}
+        className="flex items-center gap-2 items-center border-2 border-E6007E rounded-full px-5 py-1 font-semibold text-white hover:bg-E6007E hover:text-white transition-colors duration-300"
+      >
+        <IconDiscord />
+        {data?.watchers ?? "Discord"}
+      </button>
     </div>
   );
 }
-
