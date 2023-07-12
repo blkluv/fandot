ea7662766e0655788793bbc8ffd5e972f83f52ed
blkluv <info@blkluv.org>
2023-07-11 21:49:16 -0400

    updated socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,14 @@
 import GitHub from "@/components/IconGithub.tsx";
-// Import or define the necessary icon components
-import TwitterIcon from "@/components/TwitterIcon.tsx";
-import DiscordIcon from "@/components/DiscordIcon.tsx";
-import LinkedInIcon from "@/components/LinkedInIcon.tsx";
-import TikTokIcon from "@/components/TikTokIcon.tsx";
-import InstagramIcon from "@/components/InstagramIcon.tsx";
 
 export function Footer() {
   return (
     <footer class="w-11/12 max-w-5xl mx-auto mt-24 sm:!mt-28 mb-8 flex flex-col items-center justify-between">
       <div class="flex items-center gap-4 mb-4">
-        <a href="https://twitter.com/arvrtise"><TwitterIcon /> Twitter</a>
-        <a href="https://discord.gg/nfAmqTGNAU"><DiscordIcon /> Discord</a>
-        <a href="https://tiktok.com/arvrtise"><TikTokIcon /> TikTok</a>
-        <a href="https://instagram.com/arvrtise"><InstagramIcon /> Instagram</a>
-        <a href="https://threads.net/@arvrtise"><InstagramIcon /> Threads</a>
-        <a href="https://linkedin.com/showcase/89771536"><LinkedInIcon /> LinkedIn Company Page</a>
-        <a href="https://www.linkedin.com/groups/9068432/"><LinkedInIcon /> LinkedIn Group Page</a>
+        <a href="https://twitter.com/example">Twitter</a>
+        <a href="https://discord.gg/example">Discord</a>
+        <a href="https://linkedin.com/company/example">LinkedIn</a>
+        <a href="https://tiktok.com/@example">TikTok</a>
+        <a href="https://instagram.com/example">Instagram</a>
       </div>
       <div class="flex items-center gap-4">
         <iframe src="https://arvrtise.substack.com/embed" width="380" height="320" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
