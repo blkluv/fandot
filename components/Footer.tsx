c9a4d5d4daa44b04891af2b5ececfab706ba75c1
blkluv <info@blkluv.org>
2023-07-11 21:31:17 -0400

    sub

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,7 @@ import GitHub from "@/components/IconGithub.tsx";
 
 export function Footer() {
   return (
-    <footer class="w-11/12 max-w-5xl mx-auto mt-24 sm:!mt-28 mb-8 flex items-center justify-between">
-      <span class="flex items-center gap-4">
-         <iframe src="https://arvrtise.substack.com/embed" style="border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;" frameborder="0" scrolling="no"></iframe>
-      </span>
+    <footer class="w-11/12 max-w-5xl mx-auto mt-24 sm:!mt-28 mb-8 flex flex-col items-center justify-between">
       <a class="text-sm text-gray-400 hidden items-center gap-2 sm:!flex" href="https://ads.arvrtise.com">
         Powered by
         <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width={28 / 32 * 20} height={20} preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256">
@@ -15,6 +12,9 @@ export function Footer() {
           Arvrtise Ads
         </span>
       </a>
+      <div class="flex items-center gap-4">
+        <iframe src="https://arvrtise.substack.com/embed" width="380" height="320" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
+      </div>
     </footer>
   );
 }
