3cb6f34529f264e39cb7cb34b39e9c0009737305
blkluv <info@blkluv.org>
2023-07-11 21:22:07 -0400

    substack embed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,12 @@ export function Footer() {
   return (
     <footer class="w-11/12 max-w-5xl mx-auto mt-24 sm:!mt-28 mb-8 flex items-center justify-between">
       <span class="flex items-center gap-4">
-        <iframe src="https://arvrtise.substack.com/embed" width="480" height="320" style="border:1px solid #EEE; background:black;" frameborder="0" scrolling="no"></iframe>
+      <div style="max-width: 100%; overflow: hidden;">
+  <div style="padding-top: 66.67%; position: relative;">
+    <iframe src="https://arvrtise.substack.com/embed" style="border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;" frameborder="0" scrolling="no"></iframe>
+  </div>
+</div>
+
       </span>
       <a class="text-sm text-gray-400 hidden items-center gap-2 sm:!flex" href="https://ads.arvrtise.com">
         Powered by
