7346bc81b211e47906c97641eb51041ab6df920b
blkluv <info@blkluv.org>
2023-07-11 21:23:37 -0400

    footer embed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,7 @@ export function Footer() {
   return (
     <footer class="w-11/12 max-w-5xl mx-auto mt-24 sm:!mt-28 mb-8 flex items-center justify-between">
       <span class="flex items-center gap-4">
-      <div style="max-width: 100%; overflow: hidden;">
-  <div style="padding-top: 66.67%; position: relative;">
-    <iframe src="https://arvrtise.substack.com/embed" style="border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;" frameborder="0" scrolling="no"></iframe>
-  </div>
-</div>
-
+         <iframe src="https://arvrtise.substack.com/embed" style="border: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%;" frameborder="0" scrolling="no"></iframe>
       </span>
       <a class="text-sm text-gray-400 hidden items-center gap-2 sm:!flex" href="https://ads.arvrtise.com">
         Powered by
